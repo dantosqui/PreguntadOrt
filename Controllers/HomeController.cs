d10d396dad7b8fc320a7da726e86f15dfbcd6099
DANTE VERDI GUTIERREZ <47573312@ort>
2023-08-07 08:01:19 -0300

    primer commit :D

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PreguntadOrt.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
